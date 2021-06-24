afbb08ab7d38464567110d8456e7ed704998073b
abhishekm89 <magaji.abhishek@gmail.com>
2021-06-24 11:24:49 +0530

    Version 1.1

--- 2. if_statement.sh
+++ 2. if_statement.sh
@@ -1,19 +1,20 @@
-#1 /bin/bash
+#! /bin/bash
 
 
-age=2
+age=20
 
 
 # if [[ "$age" -gt 18 && "$age" -lt 40 ]]
+# OR 
 
 if [ "$age" -gt 18 ] && [ "$age" -lt 40 ]
 then
-    echo "You are an Adult"
+   $echo "You are an Adult."
 
 elif [ "$age" -gt 5 ] && [ "$age" -lt 18 ]
 then
-    echo "You are not an Adult"
-
+    echo "You are a Teenager."
+.
 else
     echo "Hello Kiddo!"
 fi
--- 3. for_loops.sh
+++ 3. for_loops.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# for i in 1 2 3 4 5
+
+#OR
+
+# for i in {0..50..2} 
+
+# {start..end..increment}
+
 for (( i=0; i<10; i++ ))
 do
     if [ $i -eq 3 ] || [ $i -eq 7 ]
